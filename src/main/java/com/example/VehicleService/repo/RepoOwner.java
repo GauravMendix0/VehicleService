e3f9a06fca5aae28fc2894fc3e7d9625d7d5bfbd
GauravMendix0 <gaurav.gokhale@mendix.com>
2025-06-24 14:09:56 +0530

    chore(RepoOwner): Add repository for owner class

---
+++ src/main/java/com/example/VehicleService/repo/RepoOwner.java
@@ -0,0 +1,8 @@
+package com.example.VehicleService.repo;
+
+import com.example.VehicleService.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepoOwner extends JpaRepository<Owner,Integer> {
+
+}
